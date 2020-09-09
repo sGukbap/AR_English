97d1745dccf6eda32c01c84578213de044fef645
sGukbap <mnomad1612@gmail.com>
2020-09-10 05:41:49 +0900

    프로토타입 버전
    + 기본적인 UI
    + 3D 모드
    + AR 모드

---
+++ Assets/Scripts/AR/MarkerMulti.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[System.Serializable]
+public class MarkerInfo
+{
+    public string name;
+    public GameObject gobj;
+}
+
+// 다수개의 마커를 인식하고 싶다.
+// 마커와 마커가 추적되었을때 보여질 GameObject
+public class MarkerMulti : MonoBehaviour
+{
+    public MarkerInfo[] markerList;
+    public ARTrackedImageManager manager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private void OnEnable()
+    {
+        manager.trackedImagesChanged += OntrackedImagesChanged;
+    }
+
+    private void OnDisable()
+    {
+        manager.trackedImagesChanged -= OntrackedImagesChanged;
+    }
+
+    private void OntrackedImagesChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        // 검출된 마커를 다 검사해서
+        for (int i = 0; i < args.updated.Count; i++)
+        {
+            ARTrackedImage image = args.updated[i];
+            // 만약 검출된 마커의 이름과 내가 보유한 마커(markerList)의 이름이 같다면
+            for (int j = 0; j < markerList.Length; j++)
+            {
+                if (image.referenceImage.name == markerList[j].name)
+                {
+                    //  만약 그 마커가 추적중이라면
+                    if (image.trackingState == TrackingState.Tracking)
+                    {
+                        // 그 마커의 게임오브젝트를 활성화하고
+                        markerList[j].gobj.SetActive(true);
+                        // 위치도 동기화 하고싶다.
+                        markerList[j].gobj.transform.position = image.transform.position;
+                    }
+                    //  그렇지않고 추적중이 아니라면
+                    else
+                    {
+                        // 그 마커의 게임오브젝트를 비활성화 하고싶다.
+                        markerList[j].gobj.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Runtime;
+
+public class ButtonManager : MonoBehaviour
+{
+    public GameObject[] obj; // 누르면 뜨게될 오브젝트 변수 배열
+    
+
+
+    public void ActiveObj()
+    {
+        // 오브젝트 배열의 길이만큼 검사한다
+        for (int i = 0; i < obj.Length; i++)
+        {
+            // 만약 선택된 버튼과 같은 이름의 오브젝트가 있으면
+            if ( EventSystem.current.currentSelectedGameObject.name == obj[i].name)
+            {
+                // 활성화한다
+                obj[i].SetActive(true);
+            }
+            // 그렇지않고 다른 이름의 오브젝트들은
+            else
+            {
+                obj[i].SetActive(false);
+            }
+        }
+    }
+}
